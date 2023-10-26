e55a364e4fc7b9223e03a6a6eecacabb47b2c5ba
MarvinGago <144975718+MarvinGago@users.noreply.github.com>
2023-10-25 23:57:37 -0600

    Add files via upload

---
+++ Caso_1/src/caso_1/Caso_1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package caso_1;
+
+/**
+ *
+ * @author CPUw1010
+ */
+public class Caso_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ Caso_1/src/caso_1/Factura.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package caso_1;
+
+/**
+ *
+ * @author CPUw1010
+ */
+public class Factura {
+    
+}
